eb50bbb9b8f9d432e48591eae8ba9d3ea8de2d9e
anhnv-hblab <132766412+anhnv-hblab@users.noreply.github.com>
2025-07-24 18:07:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('Load Main.js');
 $p.events.on_grid_load = function() {
     for (let i = 0; i < $p.events.on_grid_load_arr.length; i++) {
-        console.log( $p.events.on_grid_load_arr[i]);
-        $p.events.on_grid_load_arr[i] ();
+        console.log(i);
+        $p.events.on_grid_load_arr[i]();
     }
 }
