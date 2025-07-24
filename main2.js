a5903fc45bdd81856313d3acecf055aebc786b70
anhnv-hblab <132766412+anhnv-hblab@users.noreply.github.com>
2025-07-24 18:14:55 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -3,3 +3,8 @@ for (let i = 0; i < $p.events.on_grid_load_arr.length; i++) {
     $p.events.on_grid_load_arr[i]();
 }
 
+$p.events.on_grid_load = function() {
+    for (let i = 0; i < $p.events.on_grid_load_arr.length; i++) {
+        $p.events.on_grid_load_arr[i] ();
+    }
+}
