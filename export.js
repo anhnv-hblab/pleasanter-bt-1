b7def5c6acaceb94ae3a4e32d970f131397c2985
anhnv-hblab <132766412+anhnv-hblab@users.noreply.github.com>
2025-07-24 17:54:58 +0700

    Update export.js

--- export.js
+++ export.js
@@ -1,3 +1,4 @@
+console.log('Load export.js');
 $p.events.on_grid_load_arr.push(function() {
     $("#MainCommands").append(
         $('<button id="button-export" class="button button-icon button-neutral ui-button ui-corner-all ui-widget applied" type="button" onclick="exportDataFromSiteId()" data-icon="ui-icon-circle-arrow-e"><span class="ui-button-icon-space"> </span>Export to Excel</button>')
